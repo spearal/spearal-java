562ff8ee2d20963967b368b9a95fa52e71a899b8
fwolff <fwolff@graniteds.org>
2014-09-03 17:53:56 +0200

    Add missing headers.

--- src/main/java/org/spearal/SpearalFactory.java
+++ src/main/java/org/spearal/SpearalFactory.java
@@ -1,9 +1,29 @@
+/**
+ * == @Spearal ==>
+ * 
+ * Copyright (C) 2014 Franck WOLFF & William DRAI (http://www.spearal.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.spearal;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+/**
+ * @author William DRAI
+ */
 public interface SpearalFactory {
 
 	SpearalContext getContext();
@@ -15,5 +35,4 @@ public interface SpearalFactory {
 	SpearalDecoder newDecoder(InputStream in);
 
 	SpearalPrinter newPrinter(PrintStream out);
-
 }
